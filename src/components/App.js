cd3bdd090025a5c884c70acbe050dc68da3d9b26
ingryong <bbgod2@gmail.com>
2021-09-29 11:02:24 +0900

    구글/깃헙 소셜 로그인

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AppRouter from 'components/Router';
 import { authService } from 'fbase';
 
 function App() {
+  const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser); // useState(currentUser)를 통해 로그인 상태 여부 확인
+  useEffect(() => {
+    // hooks
+    authService.onAuthStateChanged((user) => {
+      // onAuthStateChanged -> 유저의 로그인 상태를 지속적으로 체크함
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : 'Initiializing...'}
       <footer>&copy;{new Date().getFullYear()} mywitter</footer>
     </>
   );
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { authService } from 'fbase';
+import { authService, firebaseInstance } from 'fbase';
 
 const Auth = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState('');
   const onChange = (event) => {
     const {
       target: { name, value },
@@ -16,11 +17,11 @@ const Auth = () => {
     }
   };
   const onSubmit = async (event) => {
-    event.preventDefault();
+    event.preventDefault(); // preventDefault -> 해당 이벤트의 기본 이벤트를 취소한다. input의 경우 입력 취소
     try {
       let data;
       if (newAccount) {
-        // create account
+        // 계정생성
         data = await authService.createUserWithEmailAndPassword(
           email,
           password
@@ -31,9 +32,32 @@ const Auth = () => {
       }
       console.log(data);
     } catch (error) {
-      console.log(error);
+      setError(error.message);
     }
   };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+
+  // 소셜로그인
+  const onSocialClick = async (event) => {
+    // await을 만나기 전까지 진행
+    const {
+      target: { name },
+    } = event; // 이벤트를 실행한 타겟의 name태그를 찾는다.
+
+    let provider;
+
+    if (name === 'google') {
+      // 구글을 클릭했을 경우, provider 안에 firebase의 구글 소셜로그인 api를 담는다.
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === 'github') {
+      // 깃헙을 클릭했을 경우, provider 안에 firebase의 깃헙 소셜로그인 api를 담는다.
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    }
+    // await을 통해 async의 순서(provider에 값이 담길 때까지)를 기다린 후 signInWithPopup(팝업형태의 signIn) 진행
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
+  };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -54,10 +78,18 @@ const Auth = () => {
           onChange={onChange}
         />
         <input type="submit" value={newAccount ? 'Create Account' : 'Log In'} />
+        <div>{error}</div>
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? 'Sign In' : 'Create Account'}
+      </span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with Github
+        </button>
       </div>
     </div>
   );
